2f1e92b0b10866c706236fc9d99abf98b3dbbbf1
Ian Santana <iansantana02@gmail.com>
2023-02-15 23:23:01 -0300

    Novos ajustes

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -13,14 +13,18 @@ describe('Funcionalidade Pré Cadastro', () => {
     });
 
     it('Deve completar o pré cadastro com sucesso', () => {
-        cy.get('#reg_email').type(faker.internet.email());
+        let nomeFaker = faker.name.firstname();
+        let sobrenomeFaker = faker.internet.lastname();
+        let emailFaker = faker.internet.email(nomeFaker);
+
+        cy.get('#reg_email').type(emailFaker);
         cy.get('#reg_password').type('teste@teste.com');
         cy.get(':nth-child(4) > .button').click();
         cy.contains('auto_teste_ian_00 !');
         cy.contains('A partir do painel de controle de sua conta, você pode ver suas compras recentes, gerenciar seus endereços de entrega e faturamento, e editar sua senha e detalhes da conta.');
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click();
-        cy.get('#account_first_name').type(faker.name.firstname());
-        cy.get('#account_last_name').type(faker.name.lastname());
+        cy.get('#account_first_name').type(nomeFaker);
+        cy.get('#account_last_name').type(sobrenomeFaker);
         cy.get('.woocommerce-Button').click();
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso');
     })
---
+++ cypress/e2e/produtos.cy.js
@@ -0,0 +1,33 @@
+/// <reference types = "cypress" />
+var faker = require('faker');
+
+describe('Funcionalidade Página de Produtos', () => {
+
+    beforeEach(() => {
+        cy.viewport(1600,900);
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/');
+    });
+    
+    afterEach(() => {
+        cy.screenshot();
+    });
+
+    it('Deve selecionar o produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            .contains('Ariel Roll Sleeve Sweatshrit')
+            .click();
+    })
+
+    it('Deve adicionar um produto ao carrinho', () => {
+        var quantidade = 3;
+        cy.get('[class="product-block grid"]')
+            .contains('Ariel Roll Sleeve Sweatshrit').click();
+        cy.get('.button-variable-item-M').click();
+        cy.get('.button-variable-item-Purple').click();
+        cy.get('.input-text').clear().type(quantidade);
+        cy.get('.single_add_to_cart_button').click();
+
+        cy.get('.dropdown-toogle > .mini-cart-items').should('contain' , quantidade);
+        cy.get('.woocommerce-message').should('contain', quantidade + ' x "Ariel Roll Sleeve Sweatshirt" foram adicionados no seu carrinho.');
+    })
+})
