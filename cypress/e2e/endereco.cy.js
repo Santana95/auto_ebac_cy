8d34bd484c45660ff634890e7112081b7e513597
Ian Santana <iansantana02@gmail.com>
2023-02-16 16:22:46 -0300

    Alterações da aula 07 - Page Objects

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,4 +1,5 @@
 /// <reference types = "cypress" />
+import enderecoPage from "../support/page-objects/endereco.page";
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
 
@@ -11,6 +12,7 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     })
 
     it('Deve fazer cadastro de faturamento com sucesso', () =>{
-        //
+        enderecoPage.editarEnderecoFaturamento('Ian', 'Santana', 'Google', 'Brasil', 'Rua 01', '123', 'Sorocaba' , 'São Paulo', '1234567', '11987654321', 'email@dominio.com');
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.');
     })
 })
