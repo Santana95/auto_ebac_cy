90e89f0e11d6e2e76b6f4910dc6a4760ec08c3ed
Ian Santana <iansantana02@gmail.com>
2023-02-16 17:16:25 -0300

    Alterações da aula 09 - Relatórios

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'fkrnpc',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    
+    projectId: "fkrnpc"
+        // The rest of the Cypress config options go here...
     },
   },
 });
